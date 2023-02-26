0fd4c6145a0080f6f12c82354786281297c8fed7
Fernando Silva <fdograph@gmail.com>
2023-02-26 12:38:56 +0100

    env variable for basepath in action

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const basePath = process.env.VITE_BASE_PATH ?? '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pathfinder/',
+  base: basePath,
   plugins: [react()],
 });
